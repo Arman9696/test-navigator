fc7cffc4a60811c5f71f323c1ca0dbc7caeaa5aa
a.sargsyan <a.sargasyan@iq-adv.ru>
2020-08-18 12:39:56 +0500

    NAV-7| Страница 'Компания'

--- public/about/index.php
+++ public/about/index.php
@@ -14,5 +14,49 @@ $APPLICATION->SetTitle("О компании");
 <?
 require_once $_SERVER['DOCUMENT_ROOT'] .'/_inc/menu.php';
 ?>
+<?
+global $arrFilter;
+$arrFilter = [
+    'SECTION_ID' => \IQDEV\Base\Helper::getIblockSectionId('about','about'),
+]
+?>
+
+<?$APPLICATION->IncludeComponent(
+    "bitrix:news",
+    "page-about",
+    Array(
+        "ACTIVE_DATE_FORMAT" => "d.m.Y",
+        "DETAIL_ACTIVE_DATE_FORMAT" => "d.m.Y",
+        "DETAIL_PAGER_TEMPLATE" => "",
+        "DETAIL_PAGER_TITLE" => "Страница",
+        "DETAIL_PROPERTY_CODE" => array("",""),
+        "DETAIL_SET_CANONICAL_URL" => "N",
+        "DISPLAY_BOTTOM_PAGER" => "Y",
+        "DISPLAY_DATE" => "N",
+        "DISPLAY_NAME" => "Y",
+        "DISPLAY_TOP_PAGER" => "N",
+        "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+        "IBLOCK_ID" => \IQDEV\Base\Helper::getIblockId('about'),
+        "IBLOCK_TYPE" => "content",
+        "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+        "LIST_ACTIVE_DATE_FORMAT" => "d.m.Y",
+        "NEWS_COUNT" => "3",
+        "PAGER_BASE_LINK_ENABLE" => "N",
+        "PAGER_DESC_NUMBERING" => "N",
+        "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+        "PAGER_SHOW_ALL" => "N",
+        "PAGER_SHOW_ALWAYS" => "N",
+        "PAGER_TEMPLATE" => ".default",
+        "PAGER_TITLE" => "Новости",
+        "PREVIEW_TRUNCATE_LEN" => "",
+        "SEF_FOLDER" => "/about/our-news/",
+        "SEF_MODE" => "Y",
+        "SEF_URL_TEMPLATES" =>
+            ["detail"=>"#ELEMENT_ID#/"],
+        "USE_FILTER"=>"Y",
+        "FILTER_NAME"=> '$arrFilter'
+
+    )
+);?>
 <?php
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");
