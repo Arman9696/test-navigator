084a68ffda2bde88f7d33c5a018046dafae2fa66
a.sargsyan <a.sargasyan@iq-adv.ru>
2020-08-18 11:01:15 +0500

    NAV-5 | Исправление Codestyle

---
+++ public/local/templates/.default/components/bitrix/news/page-rabota/bitrix/news.list/.default/result_modifier.php
@@ -0,0 +1,29 @@
+<?php
+CModule::IncludeModule('iqdev.options');
+$arSelect = [
+    "ID",
+    "IBLOCK_ID",
+    "IBLOCK_NAME",
+    "NAME",
+    "PREVIEW_PICTURE",
+    "PREVIEW_TEXT",
+    "PROPERTY_*",
+    "PHOTO",
+    'LIST_PROPERTY_CODE'=> [
+
+    ],
+];
+$arFilter = [
+    "IBLOCK_ID" => \IQDEV\Base\Helper::getIblockId('WHY'),
+];
+$Baners   = CIBlockElement::GetList(["SORT" => "ASC"], $arFilter, false, ["nPageSize" => 50], $arSelect);
+$Item     = [];
+$index    = 0;
+
+while ($ar_result = $Baners->GetNextElement()) {
+    $Item[$index] = $ar_result->GetFields();
+
+    $Item[$index]["PROPERTIES"] = $ar_result->GetProperties();
+    $index++;
+}
+$arResult['WHY'] = $Item;
--- public/local/templates/.default/components/bitrix/news/page-rabota/bitrix/news.list/.default/template.php
+++ public/local/templates/.default/components/bitrix/news/page-rabota/bitrix/news.list/.default/template.php
@@ -9,7 +9,8 @@ require_once $_SERVER['DOCUMENT_ROOT'] .'/_inc/menu.php';
 <section class="section mb-large">
     <div class="container">
         <div class="job-page-title">«Навигатор» – молодая, энергичная, динамично развивающаяся компания. Cтроим новые
-            коттеджные поселки, создаем инновационные проект для города,растем и развиваемся. Мы ищем лучших профессионалов в
+            коттеджные поселки, создаем инновационные проект для города,растем и развиваемся.
+            Мы ищем лучших профессионалов в
             дружную компанию. И, поверьте, Вам у нас точно понравится!
         </div>
         <div class="accordion-container js-accordion">
@@ -19,8 +20,12 @@ require_once $_SERVER['DOCUMENT_ROOT'] .'/_inc/menu.php';
                 <div class="accordion__header">
                     <div class="accordion__header-icon">
                         <svg xmlns="http://www.w3.org/2000/svg" width="13" height="9" viewBox="0 0 13 9" fill="none">
-                            <path
-                                    d="M12.8261 4.09762C13.0213 4.29288 13.0213 4.60946 12.8261 4.80473L9.64409 7.98671C9.44882 8.18197 9.13224 8.18197 8.93698 7.98671C8.74172 7.79144 8.74172 7.47486 8.93698 7.2796L11.7654 4.45117L8.93698 1.62274C8.74172 1.42748 8.74172 1.1109 8.93698 0.915638C9.13224 0.720376 9.44882 0.720376 9.64409 0.915638L12.8261 4.09762ZM0.0518303 3.95117H12.4725V4.95117H0.0518303V3.95117Z"></path>
+                            <path d="M12.8261 4.09762C13.0213 4.29288 13.0213 4.60946 12.8261 4.80473L9.64409
+                            7.98671C9.44882 8.18197 9.13224 8.18197 8.93698 7.98671C8.74172 7.79144 8.74172
+                            7.47486 8.93698 7.2796L11.7654 4.45117L8.93698 1.62274C8.74172 1.42748 8.74172
+                            1.1109 8.93698 0.915638C9.13224 0.720376 9.44882 0.720376 9.64409 0.915638L12.8261
+                            4.09762ZM0.0518303 3.95117H12.4725V4.95117H0.0518303V3.95117Z">
+                            </path>
                         </svg>
                     </div>
                     <div class="accordion__header-text">
@@ -72,14 +77,20 @@ require_once $_SERVER['DOCUMENT_ROOT'] .'/_inc/menu.php';
                     </div>
                 </div>
             </div>
-                <?} else {
-                    if ($arKey % 2 !=0){?>
+                    <?} else {
+                        if ($arKey % 2 !=0) {?>
                         <div class="accordion accordion--sand">
                             <div class="accordion__header">
                                 <div class="accordion__header-icon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="13" height="9" viewBox="0 0 13 9" fill="none">
-                                        <path
-                                                d="M12.8261 4.09762C13.0213 4.29288 13.0213 4.60946 12.8261 4.80473L9.64409 7.98671C9.44882 8.18197 9.13224 8.18197 8.93698 7.98671C8.74172 7.79144 8.74172 7.47486 8.93698 7.2796L11.7654 4.45117L8.93698 1.62274C8.74172 1.42748 8.74172 1.1109 8.93698 0.915638C9.13224 0.720376 9.44882 0.720376 9.64409 0.915638L12.8261 4.09762ZM0.0518303 3.95117H12.4725V4.95117H0.0518303V3.95117Z"></path>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="13" height="9" viewBox="0 0 13 9"
+                                         fill="none">
+                                        <path d="M12.8261 4.09762C13.0213 4.29288 13.0213 4.60946 12.8261
+                                        4.80473L9.64409 7.98671C9.44882 8.18197 9.13224 8.18197 8.93698
+                                        7.98671C8.74172 7.79144 8.74172 7.47486 8.93698 7.2796L11.7654
+                                        4.45117L8.93698 1.62274C8.74172 1.42748 8.74172 1.1109 8.93698
+                                        0.915638C9.13224 0.720376 9.44882 0.720376 9.64409 0.915638L12.8261
+                                        4.09762ZM0.0518303 3.95117H12.4725V4.95117H0.0518303V3.95117Z">
+                                        </path>
                                     </svg>
                                 </div>
                                 <div class="accordion__header-text">
@@ -99,7 +110,8 @@ require_once $_SERVER['DOCUMENT_ROOT'] .'/_inc/menu.php';
                                                 <?=$arElement['PROPERTIES']['TREBOVANIA']['NAME']?>
                                             </div>
                                             <ul class="job-section__list">
-                                                <?foreach ($arElement['PROPERTIES']['TREBOVANIA']['VALUE'] as $arProperty) {?>
+                                                <?foreach ($arElement['PROPERTIES']['TREBOVANIA']
+                                                           ['VALUE'] as $arProperty) {?>
                                                     <li><?=$arProperty?></li >
                                                 <?}?>
                                             </ul>
@@ -109,7 +121,8 @@ require_once $_SERVER['DOCUMENT_ROOT'] .'/_inc/menu.php';
                                                 <?=$arElement['PROPERTIES']['TASKS']['NAME']?>
                                             </div>
                                             <ul class="job-section__list">
-                                                <?foreach ($arElement['PROPERTIES']['TASKS']['VALUE'] as $arProperty) {?>
+                                                <?foreach ($arElement['PROPERTIES']['TASKS']
+                                                           ['VALUE'] as $arProperty) {?>
                                                     <li><?=$arProperty?></li >
                                                 <?}?>
                                             </ul>
@@ -119,25 +132,34 @@ require_once $_SERVER['DOCUMENT_ROOT'] .'/_inc/menu.php';
                                                 <?=$arElement['PROPERTIES']['REQUIREMENT']['NAME']?>
                                             </div>
                                             <ul class="job-section__list">
-                                                <?foreach ($arElement['PROPERTIES']['REQUIREMENT']['VALUE'] as $arProperty) {?>
+                                                <?foreach ($arElement['PROPERTIES']['REQUIREMENT']
+                                                           ['VALUE'] as $arProperty) {?>
                                                     <li><?=$arProperty?></li >
                                                 <?}?>
                                             </ul>
                                         </div>
                                     </div>
                                     <div class="job__footer">
-                                        <div class="button button--primary button--auto-width"><span>Отправить резюме</span></div>
+                                        <div class="button button--primary button--auto-width">
+                                            <span>Отправить резюме</span>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    <?} elseif ($arKey % 2 ==0) {?>
+                        <?} elseif ($arKey % 2 ==0) {?>
                         <div class="accordion">
                             <div class="accordion__header">
                                 <div class="accordion__header-icon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="13" height="9" viewBox="0 0 13 9" fill="none">
-                                        <path
-                                                d="M12.8261 4.09762C13.0213 4.29288 13.0213 4.60946 12.8261 4.80473L9.64409 7.98671C9.44882 8.18197 9.13224 8.18197 8.93698 7.98671C8.74172 7.79144 8.74172 7.47486 8.93698 7.2796L11.7654 4.45117L8.93698 1.62274C8.74172 1.42748 8.74172 1.1109 8.93698 0.915638C9.13224 0.720376 9.44882 0.720376 9.64409 0.915638L12.8261 4.09762ZM0.0518303 3.95117H12.4725V4.95117H0.0518303V3.95117Z"></path>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="13" height="9" viewBox="0 0 13 9"
+                                         fill="none">
+                                        <path d="M12.8261 4.09762C13.0213 4.29288 13.0213 4.60946 12.8261
+                                        4.80473L9.64409 7.98671C9.44882 8.18197 9.13224 8.18197 8.93698
+                                        7.98671C8.74172 7.79144 8.74172 7.47486 8.93698 7.2796L11.7654
+                                        4.45117L8.93698 1.62274C8.74172 1.42748 8.74172 1.1109 8.93698
+                                        0.915638C9.13224 0.720376 9.44882 0.720376 9.64409 0.915638L12.8261
+                                        4.09762ZM0.0518303 3.95117H12.4725V4.95117H0.0518303V3.95117Z">
+                                        </path>
                                     </svg>
                                 </div>
                                 <div class="accordion__header-text">
@@ -157,7 +179,8 @@ require_once $_SERVER['DOCUMENT_ROOT'] .'/_inc/menu.php';
                                                 <?=$arElement['PROPERTIES']['TREBOVANIA']['NAME']?>
                                             </div>
                                             <ul class="job-section__list">
-                                                <?foreach ($arElement['PROPERTIES']['TREBOVANIA']['VALUE'] as $arProperty) {?>
+                                                <?foreach ($arElement['PROPERTIES']['TREBOVANIA']
+                                                           ['VALUE'] as $arProperty) {?>
                                                     <li><?=$arProperty?></li >
                                                 <?}?>
                                             </ul>
@@ -167,7 +190,8 @@ require_once $_SERVER['DOCUMENT_ROOT'] .'/_inc/menu.php';
                                                 <?=$arElement['PROPERTIES']['TASKS']['NAME']?>
                                             </div>
                                             <ul class="job-section__list">
-                                                <?foreach ($arElement['PROPERTIES']['TASKS']['VALUE'] as $arProperty) {?>
+                                                <?foreach ($arElement['PROPERTIES']['TASKS']
+                                                           ['VALUE'] as $arProperty) {?>
                                                     <li><?=$arProperty?></li >
                                                 <?}?>
                                             </ul>
@@ -177,23 +201,63 @@ require_once $_SERVER['DOCUMENT_ROOT'] .'/_inc/menu.php';
                                                 <?=$arElement['PROPERTIES']['REQUIREMENT']['NAME']?>
                                             </div>
                                             <ul class="job-section__list">
-                                                <?foreach ($arElement['PROPERTIES']['REQUIREMENT']['VALUE'] as $arProperty) {?>
+                                                <?foreach ($arElement['PROPERTIES']['REQUIREMENT']
+                                                           ['VALUE'] as $arProperty) {?>
                                                     <li><?=$arProperty?></li >
                                                 <?}?>
                                             </ul>
                                         </div>
                                     </div>
                                     <div class="job__footer">
-                                        <div class="button button--primary button--auto-width"><span>Отправить резюме</span></div>
+                                        <div class="button button--primary button--auto-width">
+                                            <span>Отправить резюме</span>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    <?}
-                }?>
+                        <?}
+                    }?>
             <?}?>
         </div>
         </div>
     </div>
 </section>
+<section class="section mb-large">
+    <div class="container">
+        <div class="section-header">
+            <?=$arResult['WHY'][0]['IBLOCK_NAME']?>
+        </div>
+        <?foreach ($arResult['WHY'] as $arElement) {?>
+        <div class="vacancy-description">
+            <div class="vacancy-description__item vacancy-description__item--gallery">
+                <div class="vacancy-description-gallery js-vacancy-description-gallery">
+                    <div class="vacancy-description-gallery__image"><img/></div>
+                    <div class="vacancy-description-gallery__slider vacancy-description-gallery__slider--left">
+                        <div class="glide">
+                            <div class="glide__track" data-glide-el="track">
+                                <ul class="glide__slides">
+                                        <?foreach ($arElement['PROPERTIES']['PHOTO']['VALUE'] as $arKey => $iPhoto) {?>
+                                    <li class="glide__slide">
+                                        <img src="<?=CFile::GetPath($iPhoto)?>" data-index="<?=$arKey?>"/></li>
+                                        <?}?>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
+            <div class="vacancy-description__item vacancy-description__item--text">
+                <div class="vacancy-description-text">
+                    <div class="vacancy-description-text__title"><?=$arElement['NAME']?></div>
+                    <div class="vacancy-description-text__text-blocks">
+                        <?=$arElement['PREVIEW_TEXT']?>
+                    </div>
+                </div>
+            </div>
+        <?}?>
+        </div>
+</section>
+<?
+require_once $_SERVER['DOCUMENT_ROOT'] .'/_inc/line-unit.php';
