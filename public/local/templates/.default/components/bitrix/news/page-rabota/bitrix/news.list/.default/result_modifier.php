16b3f1a9ede228020284a209dcdca1a847fe267b
a.sargsyan <a.sargasyan@iq-adv.ru>
2020-08-18 11:28:51 +0500

    NAV-5 | Исправление Codestyle

--- public/local/templates/.default/components/bitrix/news/page-rabota/bitrix/news.list/.default/result_modifier.php
+++ public/local/templates/.default/components/bitrix/news/page-rabota/bitrix/news.list/.default/result_modifier.php
@@ -9,18 +9,16 @@ $arSelect = [
     "PREVIEW_TEXT",
     "PROPERTY_*",
     "PHOTO",
-    'LIST_PROPERTY_CODE'=> [
-
-    ],
+    'LIST_PROPERTY_CODE'
 ];
 $arFilter = [
     "IBLOCK_ID" => \IQDEV\Base\Helper::getIblockId('WHY'),
 ];
-$Baners   = CIBlockElement::GetList(["SORT" => "ASC"], $arFilter, false, ["nPageSize" => 50], $arSelect);
+$oList   = CIBlockElement::GetList(["SORT" => "ASC"], $arFilter, false, ["nPageSize" => 50], $arSelect);
 $Item     = [];
 $index    = 0;
 
-while ($ar_result = $Baners->GetNextElement()) {
+while ($ar_result = $oList->GetNextElement()) {
     $Item[$index] = $ar_result->GetFields();
 
     $Item[$index]["PROPERTIES"] = $ar_result->GetProperties();
