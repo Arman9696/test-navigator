abb19b44db7594ec794ca95606525cafb48dc5f3
a.sargsyan <a.sargasyan@iq-adv.ru>
2020-08-19 16:58:11 +0500

    NAV-11 | Страница 'Ипотека'

---
+++ public/local/templates/.default/components/bitrix/news/page-ipoteka/bitrix/news.list/.default/result_modifier.php
@@ -0,0 +1,19 @@
+<?php
+CModule::IncludeModule('iqdev.options');
+$arSelect = ["ID", "IBLOCK_ID", "NAME", "DETAIL_PICTURE", "PREVIEW_TEXT", "PROPERTY_*"];
+
+$arFilter    = [
+    "IBLOCK_ID" => \IQDEV\Base\Helper::getIblockId('rassrochka'),   // id инфоблока
+    "SECTION_ID" => \IQDEV\Base\Helper::getIblockSectionId('rassrochka', 'why_profitable'),     // нужная секция
+];
+$oRassrochka = CIBlockElement::GetList(["SORT" => "ASC"], $arFilter, false, ["nPageSize" => 50], $arSelect);
+
+$Item  = [];
+$index = 0;
+while ($ar_result = $oRassrochka->GetNextElement()) {
+    $Item[$index] = $ar_result->GetFields();
+
+    $Item[$index]["PROPERTIES"] = $ar_result->GetProperties();
+    $index++;
+}
+$arResult['rassrochka'] = $Item;
